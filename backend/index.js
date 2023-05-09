09cdaf2c7f48791038eecf2f9ac8fcc65b6d6058
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-09 10:21:24 -0400

    Made some updates to note backend

--- backend/index.js
+++ backend/index.js
@@ -34,16 +34,28 @@ app.get("/api/notes", (request, response) => {
 });
 
 app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
 
-  response.status(204).end();
+    .catch((error) => error);
 });
 
 // const generateId = () => {
