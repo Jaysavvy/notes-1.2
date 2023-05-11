5ea8f5755e0c3a7627152c6b144c522a5ab10c5f
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-11 18:40:41 -0400

    Added a validation rule to POST Route

--- backend/index.js
+++ backend/index.js
@@ -63,9 +63,9 @@ app.delete("/api/notes/:id", (request, response) => {
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  // if (body.content === undefined) {
-  //   return response.status(400).json({ error: "content missing" });
-  // }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
 
   const note = new Note({
     content: body.content,
