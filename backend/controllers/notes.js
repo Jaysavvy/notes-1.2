7d627a619632558477f9bf2110e1a0939daebc9d
Jay Lovinsky <104098730+Jaysavvy@users.noreply.github.com>
2023-05-23 15:58:02 -0400

    Finished adding user model and adding to notes id

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -25,16 +25,21 @@ notesRouter.post("/", async (request, response, next) => {
   
   const body = request.body;
 
+  const user = await User.findById(body.userId)
+
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user.id
   });
 
-  const savedNote = await note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
+  const savedNote = await note.save()
+
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
+  response.json(savedNote)
+
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
